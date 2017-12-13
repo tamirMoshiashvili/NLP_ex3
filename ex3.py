3235d173acf74cdd7a38bed7b759027aaea70049
Tamir <Tamir@DESKTOP-8R67EMV>
2017-12-14 00:43:17 +0200

    # function for reading the data-file and parsing it # works for a small (sample) of the large file, but for a large file I get memory error

--- ex3.py
+++ ex3.py
@@ -1,6 +1,7 @@
 from time import time
 
 import numpy as np
+import sys
 
 
 def cosine(u, v):
@@ -8,18 +9,36 @@ def cosine(u, v):
     return np.dot(u, v) / (np.sqrt(np.dot(u, u)) * np.sqrt(np.dot(v, v)))
 
 
+def read_dataset_file(filename):
+    f = open(filename, 'r')
+    file_lines = f.readlines()
+    f.close()
+    return file_lines
+
+
+# get a list where each item is a dictionary
+def parse_dataset(filename):
+    file_lines = read_dataset_file(filename)
+    ls = []
+
+    for line in file_lines:
+        toks = line.split()
+        if len(toks) != 0:  # skip empty lines
+            # create dictionary out of the given line according to the format in https://depparse.uvt.nl/DataFormat.html
+            format_dict = {'id': int(toks[0]), 'form': toks[1], 'lemma': toks[2],
+                           'cpostag': toks[3], 'postag': toks[4], 'feats': toks[5],
+                           'head': toks[6], 'deprel': toks[7], 'phead': toks[8], 'pdeprel': toks[9]}
+            ls.append(format_dict)
+    return ls
+
+
 if __name__ == '__main__':
     print 'start'
 
     t = time()
 
-    # test the cosine
-    dim = 3
-    iter_num = 5
-    for i in range(iter_num):
-        a = np.random.rand(dim)
-        b = np.random.rand(dim)
-
-        print i, a, b, cosine(a, b)
+    for i, item in enumerate(parse_dataset(sys.argv[1])):
+        if i % 8000 == 0:
+            print item
 
     print 'time: ', time() - t
