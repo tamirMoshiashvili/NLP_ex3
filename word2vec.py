6d0bdefd434d2ef2707149a47c5650150683f0b0
Tamir <Tamir@DESKTOP-8R67EMV>
2017-12-26 12:38:54 +0200

    # word2vec works! # NOTE - needs pandas-package

--- word2vec.py
+++ word2vec.py
@@ -1,7 +1,6 @@
 import sys
 from time import time
-
-import numpy as np
+import pandas as pd
 
 TARGET_WORDS = ['car', 'bus', 'hospital', 'hotel', 'gun', 'bomb', 'horse', 'fox', 'table', 'bowl', 'guitar', 'piano']
 
@@ -11,12 +10,13 @@ def load_words_and_vectors():
 
     # find number of columns
     with open(filename, 'r') as f:
-        cols = len(f.readline().split())
+        num_cols = len(f.readline().split())
 
     # extract from file the words and their representation as vectors
-    vectors_matrix = np.loadtxt(filename, usecols=range(1, cols))
+    words_arr = pd.read_csv(filename, header=None, delimiter=' ', dtype=str, usecols=[0]).values
     print 'loaded words'
-    words_arr = np.loadtxt(filename, usecols=0, dtype=str)
+    vectors_matrix = pd.read_csv(filename, header=None, delimiter=' ', usecols=range(1, num_cols)).values
+    print 'loaded matrix'
 
     return words_arr, vectors_matrix
 
@@ -28,12 +28,13 @@ def calc_sim(u, words_arr, vectors_matrix):
     return words_arr[sim_ids]  # return list of similar words (each is string)
 
 
-if __name__ == '__main__':
+def main():
     t = time()
     print 'start'
 
+    # load words and vectors and create dictionary for the words
     words, matrix = load_words_and_vectors()
-    w2i = {w: i for i, w in enumerate(words)}
+    w2i = {words[i][0]: i for i in range(len(words))}
 
     print 'time for loading file', time() - t
     t = time()
@@ -41,10 +42,18 @@ if __name__ == '__main__':
     result_file = open('results/word2vec_' + sys.argv[1].split('.')[0] + '.txt', 'w')
     for word in TARGET_WORDS:  # find similar words for each of target-words
         result_file.write(word + ':\n')
-        u_vec = matrix[w2i[word]]
-        for sim_word in calc_sim(word, words, matrix):
+        word_as_vec = matrix[w2i[word]]
+
+        # find similar words to word, taking [:21] items include the word and [1:] skips it
+        sim_words = [word[0] for word in calc_sim(word_as_vec, words, matrix)[:21]][1:]
+
+        for sim_word in sim_words:
             result_file.write('\t' + sim_word + '\n')
         result_file.write('\n')
 
     result_file.close()
     print 'time for finding similarities', time() - t
+
+
+if __name__ == '__main__':
+    main()
