9693e1344f2626912d0c858e8110c9f8a1e31ce7
Tamir <Tamir@DESKTOP-8R67EMV>
2018-01-02 22:03:08 +0200

    # updated report

--- word2vec.py
+++ word2vec.py
@@ -1,6 +1,7 @@
 import sys
 from time import time
 import pandas as pd
+import numpy as np
 
 TARGET_WORDS = ['car', 'bus', 'hospital', 'hotel', 'gun', 'bomb', 'horse', 'fox', 'table', 'bowl', 'guitar', 'piano']
 
@@ -39,13 +40,13 @@ def main():
     print 'time for loading file', time() - t
     t = time()
 
-    result_file = open('results/word2vec_' + sys.argv[1].split('.')[0] + '.txt', 'w')
+    result_file = open('features/word2vec_features_' + sys.argv[1].split('.')[0] + '.txt', 'w')
     for word in TARGET_WORDS:  # find similar words for each of target-words
         result_file.write(word + ':\n')
         word_as_vec = matrix[w2i[word]]
 
         # find similar words to word, taking [:21] items include the word and [1:] skips it
-        sim_words = [word[0] for word in calc_sim(word_as_vec, words, matrix)[:21]][1:]
+        sim_words = [word[0] for word in calc_sim(word_as_vec, words, matrix)[1:11]]
 
         for sim_word in sim_words:
             result_file.write('\t' + sim_word + '\n')
@@ -53,6 +54,31 @@ def main():
 
     result_file.close()
     print 'time for finding similarities', time() - t
+
+
+def calc_sim2(word_vec):
+    d = dict()
+
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            context_vec = np.ndarray(line[1:])
+            d[line[0]] = context_vec.dot(word_vec)
+
+    return d
+
+
+def main2():
+    filename = sys.argv[1]
+    features_file = open('features/word2vec_features_' + filename.split('.')[0] + '.txt', 'w')
+    context_words = pd.read_csv(filename, header=None, delimiter=' ', dtype=str, usecols=[0]).values
+    print 'loaded words'
+
+    for target_word in TARGET_WORDS:
+        features_file.write(target_word + ':\n')
+        for feat in calc_sim2(target_word):  # todo change to word-vector
+            features_file.write('\t' + feat + '\n')
+
+    features_file.close()
 
 
 if __name__ == '__main__':
