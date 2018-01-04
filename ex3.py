f1471eb8fa2aafcd2f49f9bd161c9ea90ab9ecc6
amosmim <amosmim@gmail.com>
2018-01-04 02:34:15 +0200

    part 3 + part 6.3 files

--- ex3.py
+++ ex3.py
@@ -31,6 +31,22 @@ def write_features_to_file(part, associator):
     f.close()
     exit(0)
 
+def write_first_order_to_file(part, vector):
+    f = open('./features/first_order_for_part' + str(part) + '.txt', 'w')
+    for target_word in TARGET_WORDS:
+        f.write(target_word + ':\n')
+        vec = sorted(vector.vectors[vector.associator.get_word_id(target_word)])
+        if len(vec) > 20:
+            vec = vec[-20:]
+        else:
+            print str(len(vec))+" = " +target_word
+        for feat in vec:
+            f.write('\t' + associator.get_word_from_id(feat) + '\n')
+        f.write('\n')
+
+    f.close()
+    exit(0)
+
 
 if __name__ == '__main__':
     print 'start'
@@ -40,11 +56,13 @@ if __name__ == '__main__':
     to_test = True
 
     associator = make_association_to_part(part, sys.argv[1])
+
     vector_builder = VectorBuilder(associator)
     if to_test:
         associator.test()
         vector_builder.test_pmi()
     vector_builder.build_all_vectors()
+    write_first_order_to_file(part, vector_builder)
     vector_builder.find_similarities(TARGET_WORDS, 'result_part' + str(part) + ".txt")
 
     print 'time: ', time() - t
--- results/result_organize.py
+++ results/result_organize.py
@@ -1,33 +1,43 @@
+
+DELIM = '='
 def get_dict_for(filename):
     d = dict()
     key = None
     with open(filename, 'r') as f:
         lines = f.read().splitlines()
         for line in lines:
-            if ':' in line:
-                key = line.split(':')[0]
+            if DELIM in line:
+                key = line.split(DELIM)[0]
                 d[key] = []
             elif line:
                 d[key].append(line.strip())
     return d
 
-
+#FILE_NAME = "first_order_for_part"
+FILE_NAME = "features_word2vec_"
+#RESULT_NAME = "first_order"
+RESULT_NAME = "features_word2vec"
 if __name__ == '__main__':
-    partial_name = '../features/features_part'
+    partial_name = '../features/'+ FILE_NAME
     ending = '.txt'
-    d1 = get_dict_for(partial_name + '1' + ending)
-    d2 = get_dict_for(partial_name + '2' + ending)
-    d3 = get_dict_for(partial_name + '3' + ending)
-    ds = [d1, d2, d3]
+    #d1 = get_dict_for(partial_name + '1' + ending)
+    d1 = get_dict_for(partial_name + 'bow5' + ending)
+    #d2 = get_dict_for(partial_name + '2' + ending)
+    d2 = get_dict_for(partial_name + 'deps' + ending)
+    #d3 = get_dict_for(partial_name + '3' + ending)
+    ds = [d1, d2]
+    #ds = [d1, d2, d3]
 
-    output_file = open('../features/most_common_features.csv', 'w')
+    output_file = open('../features/most_common_'+ RESULT_NAME +'.csv', 'w')
     for key in d1:
         # sort lists of each key
         for d in ds:
             d[key].sort()
 
-        output_file.write(key + '\n')
-        for a, b, c in zip(d1[key], d2[key], d3[key]):
-            output_file.write(',' + a + ',' + b + ',' + c + '\n')
-
+        output_file.write(key + ',bow5 ,deps\n')
+        #for a, b, c in zip(d1[key], d2[key], d3[key]):
+        #    output_file.write(',' + a + ',' + b + ',' + c + '\n')
+        for a, b in zip(d1[key], d2[key]):
+            output_file.write(',' + a + ',' + b  + '\n')
+        output_file.write("\n")
     output_file.close()
